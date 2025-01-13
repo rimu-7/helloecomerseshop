70292acd49a0b1ccfd0f2f4dfc483661141d6248
rimu-7 <rrimu99@gmail.com>
2025-01-13 23:28:32 +0800

    main

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
+import Navbard from '../Pages/Shared/Navbar/Navbard';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
-import SearchBox from '../Pages/Shared/Navbar/SearchBox';
+import AIChat from '../Pages/Shared/Footer/AiChat';
+import TawkToChat from '../Pages/Home/Home/Tawk';
+import TawkToMessage from '../Pages/Home/Home/TawkToMessage';
 
 const Main = () => {
     return (
         <div className='bg-gray-50'>
             <div className='z-50'>
-            <SearchBox />
+                <Navbard />
             </div>
-            
+
             <div className='sticky top-0 z-30'>
                 <Navbar />
             </div>
-            
+
             <br />
             <div className=''>
                 <Outlet />
             </div>
+            <TawkToMessage />
             <Footer />
         </div>
     );
