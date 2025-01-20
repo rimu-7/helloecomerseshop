b266146aa12912d22c0d7dae326c8fd6eec5c2ac
rimu-7 <rrimu99@gmail.com>
2025-01-21 02:51:24 +0800

    data

---
+++ src/api/newarraival.js
@@ -0,0 +1,119 @@
+export const products = [
+  {
+    id: 1,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 2,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 3,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 4,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1651950540805-b7c71869e689?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 5,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1649261191624-ca9f79ca3fc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 6,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1649261191606-cb2496e97eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 7,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 8,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 9,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 10,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1651950540805-b7c71869e689?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 11,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1649261191624-ca9f79ca3fc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 12,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1649261191606-cb2496e97eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 13,
+    name: "Product Name",
+    brand: "Brand",
+    price: 149,
+    originalPrice: 199,
+    image:
+      "https://images.unsplash.com/photo-1649261191606-cb2496e97eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
