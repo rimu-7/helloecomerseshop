30989e7971f710cc3dfc7f4eb60bd75897824658
rimu-7 <rrimu99@gmail.com>
2025-01-21 16:34:00 +0800

    Routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -43,7 +43,7 @@ export const router = createBrowserRouter([
         element: <CustomerCare />, // Products Page
       },
       {
-        path: "/men/details",
+        path: "/details",
         element: <ProductDetails />, // Products Page
       },
       {
